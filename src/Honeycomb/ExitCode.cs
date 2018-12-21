14379623313765921fea11ca2453e6b5dac693ab
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-12-21 15:27:23 +0100

    Add exit code enum

---
+++ src/Honeycomb/ExitCode.cs
@@ -0,0 +1,23 @@
+﻿namespace Honeycomb
+{
+    /// <summary>
+    /// Enumeration of possible exit codes.
+    /// </summary>
+    public enum ExitCode
+    {
+        /// <summary>
+        /// Exit code for when the program exits correctly.
+        /// </summary>
+        Success = 0,
+
+        /// <summary>
+        /// Exit code for when no target was given.
+        /// </summary>
+        NoTargets = 1,
+
+        /// <summary>
+        /// Exit code for when a given target was not known to the program.
+        /// </summary>
+        UnknownTarget = 2
+    }
+}
