a3a1470f9f0890cf33c6481b2083f5ff1f62a85f
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-12-21 15:08:35 +0100

    Add basic project

---
+++ src/Honeycomb/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Honeycomb
+{
+    /// <summary>
+    /// Entry class of the CLI.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Entry function of the CLI.
+        /// </summary>
+        /// <param name="args">Program arguments.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
