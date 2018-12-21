096f75dd26189cc7b10c4cfd6fd79fd22c1043b9
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-12-21 17:07:51 +0100

    Add GitInfo class and pre-build events

---
+++ src/Honeycomb/GitInfo.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Honeycomb
+{
+    /// <summary>
+    /// Used to retrieve certain git information which is added during compile-time.
+    /// </summary>
+    public static class GitInfo
+    {
+        /// <summary>
+        /// Gets the hash of the current commit.
+        /// </summary>
+        /// <returns>The hash of the current commit.</returns>
+        public static string GetCommit()
+            => GetContent("commit");
+
+        /// <summary>
+        /// Gets the name of the current branch.
+        /// </summary>
+        /// <returns>The name of the current branch.</returns>
+        public static string GetBranch()
+            => GetContent("branch");
+
+        /// <summary>
+        /// Gets the url of the repository.
+        /// </summary>
+        /// <returns>The url of the repository.</returns>
+        public static string GetAddress()
+            => GetContent("url");
+
+        private static string GetContent(string fileName)
+        {
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"Honeycomb.git_info.{fileName}"))
+            {
+                if (stream == null)
+                {
+                    return "UNKNOWN";
+                }
+
+                using (StreamReader sr = new StreamReader(stream))
+                {
+                    return sr.ReadToEnd().Trim();
+                }
+            }
+        }
+    }
+}
