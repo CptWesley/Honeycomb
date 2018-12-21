40e1853e6aa23a8f941023297e4e6f997b6ceb13
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-12-21 15:35:19 +0100

    Change ExitCode to no longer be an enum due to explicit casts in the future

--- src/Honeycomb/ExitCode.cs
+++ src/Honeycomb/ExitCode.cs
@@ -1,23 +1,23 @@
 ﻿namespace Honeycomb
 {
     /// <summary>
-    /// Enumeration of possible exit codes.
+    /// Constant holder for exit codes.
     /// </summary>
-    public enum ExitCode
+    public static class ExitCode
     {
         /// <summary>
         /// Exit code for when the program exits correctly.
         /// </summary>
-        Success = 0,
+        public const int Success = 0;
 
         /// <summary>
         /// Exit code for when no target was given.
         /// </summary>
-        NoTargets = 1,
+        public const int NoTargets = 1;
 
         /// <summary>
         /// Exit code for when a given target was not known to the program.
         /// </summary>
-        UnknownTarget = 2
+        public const int UnknownTarget = 2;
     }
 }
